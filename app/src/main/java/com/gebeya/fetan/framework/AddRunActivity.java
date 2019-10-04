c07a887bd6967f19390913e5de68d97e1f2c3605
genet <genetatakilt.g@gmail.com>
2019-10-04 03:14:39 -0700

    End of lesson 10

--- app/src/main/java/com/gebeya/fetan/framework/AddRunActivity.java
+++ app/src/main/java/com/gebeya/fetan/framework/AddRunActivity.java
@@ -4,16 +4,24 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
+import androidx.room.Room;
 
 import com.gebeya.fetan.R;
 import com.gebeya.fetan.data.AppDatabase;
 import com.gebeya.fetan.framework.base.BaseActivity;
+import com.gebeya.fetan.framework.util.Const;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class AddRunActivity extends BaseActivity {
 
     private AppDatabase db;
 
+    @BindView(R.id.addRunDistanceEditText)
     EditText distanceEditText;
+
+
 
     EditText timeEditText;
 
@@ -22,10 +30,17 @@ public class AddRunActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_run);
 
+
+
       db = Room.databaseBuilder(
-              context.this,
+              this,
               AppDatabase.class,
-              Const
-      )
+              Const.DATABASE_NAME
+      ).build();
+    }
+    @OnClick(R.id.addRunSaveButton)
+    public void SaveRun()
+    {
+        toast("Run saved");
     }
 }
